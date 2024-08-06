ed09684cf4e468a296fa441736f9a79e4ce603d5
Mugdha Polimera <mugdhapolimera@dhcp-10-250-97-200.harvard.edu>
2024-08-06 13:47:28 -0400

    removing test commands

--- tests/test_jats.py
+++ tests/test_jats.py
@@ -94,10 +94,6 @@ class TestJATS(unittest.TestCase):
 
             parsed = parser.parse(input_data)
 
-            # with open(test_outfile_temp, "w") as fp:
-            #     parsed["recordData"]["parsedTime"] = ""
-            #     json.dump(parsed,fp, indent = 2, sort_keys=True)
-
             with open(test_outfile, "rb") as fp:
                 output_text = fp.read()
                 output_data = json.loads(output_text)
@@ -109,11 +105,11 @@ class TestJATS(unittest.TestCase):
                 self.fail("Schema validation failed")
 
             # this field won't match the test data, so check and then discard
-            # time_difference = (
-            #     datetime.datetime.strptime(parsed["recordData"]["parsedTime"], TIMESTAMP_FMT)
-            #     - datetime.datetime.utcnow()
-            # )
-            # self.assertTrue(abs(time_difference) < datetime.timedelta(seconds=10))
+            time_difference = (
+                datetime.datetime.strptime(parsed["recordData"]["parsedTime"], TIMESTAMP_FMT)
+                - datetime.datetime.utcnow()
+            )
+            self.assertTrue(abs(time_difference) < datetime.timedelta(seconds=10))
             parsed["recordData"]["parsedTime"] = ""
             self.assertEqual(parsed, output_data)
 
